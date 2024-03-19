67c58b4942db1eb5b08596fa195bb9539e9d04f4
hansintheair <hannesz1@gmail.com>
2024-03-18 17:41:43 -0700

    Add user accounts

---
+++ UserAccounts/build/Debug/Cygwin-Windows/UsrAccntsModel.o.d
@@ -0,0 +1,4 @@
+build/Debug/Cygwin-Windows/UsrAccntsModel.o: UsrAccntsModel.cpp \
+ UsrAccntsModel.h Account.h
+UsrAccntsModel.h:
+Account.h:
---
+++ UserAccounts/build/Debug/Cygwin-Windows/main.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin-Windows/main.o: main.cpp UsrAccntsModel.h Account.h
+UsrAccntsModel.h:
+Account.h:
