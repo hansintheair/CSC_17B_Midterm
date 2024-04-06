5ddbcbfab6a89771cd9797e4a1f6711c6bf8da3e
hansintheair <hannesz1@gmail.com>
2024-04-05 17:28:39 -0700

    initial commit

---
+++ 08_Refactor/Generics.h
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   Generics.h
+ * Author: hannes
+ *
+ * Created on April 5, 2024, 5:23 PM
+ * 
+ * Generic data and functions.
+ * 
+ */
+
+#ifndef GENERICS_H
+#define GENERICS_H
+
+#include <type_traits>
+
+// Constants
+
+// Data Abstracts
+
+// Generic helper methods
+
+
+
+
+
+#endif /* GENERICS_H */
+
---
+++ 08_Refactor/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: hanne
+ *
+ * Created on April 5, 2024, 5:21 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+
+    return 0;
+}
+
