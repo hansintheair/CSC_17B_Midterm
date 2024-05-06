b0b9f6265b799053705c70c0160ae90e822a2efa
hansintheair <hannesz1@gmail.com>
2024-05-05 17:17:27 -0700

    Update doxygen doc

--- html/search/all_2.js
+++ html/search/all_2.js
@@ -10,7 +10,8 @@ var searchData=
   ['delcatalogitem_7',['delCatalogItem',['../class_admin.html#a5cfd5123db3342b573b06e61530397dd',1,'Admin']]],
   ['delete_8',['DELETE',['../_constants_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70ba9d61e82a9a12752f10aece1b22183913',1,'Constants.h']]],
   ['deletedb_9',['deleteDB',['../class_d_b_model.html#a2903ca6e059a3916ec5adb785c0a6e74',1,'DBModel']]],
-  ['dirty_10',['DIRTY',['../_constants_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70ba6a4e132eb192a22c351397837d4c082c',1,'Constants.h']]],
-  ['display_11',['display',['../class_account.html#a1c695dc147302967a44a00b00ce1a4ad',1,'Account::display()'],['../class_catalog.html#a13f9419c1f7fa85b5924f3555662f764',1,'Catalog::display()'],['../class_d_b_model.html#acade24f4337956df14bb418e2aa0d472',1,'DBModel::display()']]],
-  ['documentation_20homepage_12',['Documentation Homepage',['../index.html',1,'']]]
+  ['delselfaccount_10',['delSelfAccount',['../class_admin.html#a696e9117148218e75e5e9fb403078ba7',1,'Admin::delSelfAccount()'],['../class_profile_base.html#a0f0e50dbda880f7a96ad56b956837d7a',1,'ProfileBase::delSelfAccount()']]],
+  ['dirty_11',['DIRTY',['../_constants_8h.html#a67a0db04d321a74b7e7fcfd3f1a3f70ba6a4e132eb192a22c351397837d4c082c',1,'Constants.h']]],
+  ['display_12',['display',['../class_account.html#a1c695dc147302967a44a00b00ce1a4ad',1,'Account::display()'],['../class_catalog.html#a13f9419c1f7fa85b5924f3555662f764',1,'Catalog::display()'],['../class_d_b_model.html#acade24f4337956df14bb418e2aa0d472',1,'DBModel::display()']]],
+  ['documentation_20homepage_13',['Documentation Homepage',['../index.html',1,'']]]
 ];
--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -6,5 +6,6 @@ var searchData=
   ['delall_3',['delAll',['../class_d_b_model.html#ac7c4055cfcac43575654de658ef90777',1,'DBModel']]],
   ['delcatalogitem_4',['delCatalogItem',['../class_admin.html#a5cfd5123db3342b573b06e61530397dd',1,'Admin']]],
   ['deletedb_5',['deleteDB',['../class_d_b_model.html#a2903ca6e059a3916ec5adb785c0a6e74',1,'DBModel']]],
-  ['display_6',['display',['../class_account.html#a1c695dc147302967a44a00b00ce1a4ad',1,'Account::display()'],['../class_catalog.html#a13f9419c1f7fa85b5924f3555662f764',1,'Catalog::display()'],['../class_d_b_model.html#acade24f4337956df14bb418e2aa0d472',1,'DBModel::display()']]]
+  ['delselfaccount_6',['delSelfAccount',['../class_admin.html#a696e9117148218e75e5e9fb403078ba7',1,'Admin::delSelfAccount()'],['../class_profile_base.html#a0f0e50dbda880f7a96ad56b956837d7a',1,'ProfileBase::delSelfAccount()']]],
+  ['display_7',['display',['../class_account.html#a1c695dc147302967a44a00b00ce1a4ad',1,'Account::display()'],['../class_catalog.html#a13f9419c1f7fa85b5924f3555662f764',1,'Catalog::display()'],['../class_d_b_model.html#acade24f4337956df14bb418e2aa0d472',1,'DBModel::display()']]]
 ];
